0951a7b56900a301b2abf632ed9448c1ff6d4ed2
KrasnovEA <32244235+KrasnovEA@users.noreply.github.com>
2021-04-05 01:00:41 +0300

    Update shortest_path.py

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -1,5 +1,5 @@
 from pyspark import SparkContext, SparkConf
-import os
+import os	
 import sys
  
 conf = SparkConf()
@@ -13,10 +13,13 @@ end = str(sys.argv[2])
 
 buf = twi_graph.filter(lambda x: x[0] == start).map(lambda x: (x[1], start))
 buf = twi_graph.join(buf).map(lambda x: (x[1][0], (x[0], x[1][1])))
-k = 0
-while k <= 30:
-    buf = twi_graph.join(buf).map(lambda x: (x[1][0], (x[0], ) + x[1][1]))
-    if buf.keys().filter(lambda x: x == end).count() > 0:
-        buf.filter(lambda x: x[0] == end).map(lambda x: ((x[0],) + x[1])[::-1]).map(lambda x: ','.join(x)).saveAsTextFile(sys.argv[4])
-        break
-    k += 1
+if buf.keys().filter(lambda x: x == end).count() > 0:
+    buf.filter(lambda x: x[0] == end).map(lambda x: ((x[0],) + x[1])[::-1]).map(lambda x: ','.join(x)).saveAsTextFile(sys.argv[4])
+else:
+    k = 0
+    while k <= 30:
+        buf = twi_graph.join(buf).map(lambda x: (x[1][0], (x[0], ) + x[1][1]))
+        if buf.keys().filter(lambda x: x == end).count() > 0:
+            buf.filter(lambda x: x[0] == end).map(lambda x: ((x[0],) + x[1])[::-1]).map(lambda x: ','.join(x)).saveAsTextFile(sys.argv[4])
+            break
+        k += 1
