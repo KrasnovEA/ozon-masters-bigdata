2f0871dc1a2e84195e2be522fd961487842382ea
KrasnovEA <32244235+KrasnovEA@users.noreply.github.com>
2021-04-05 00:26:37 +0300

    Update shortest_path.py

--- projects/3/shortest_path.py
+++ projects/3/shortest_path.py
@@ -14,11 +14,10 @@ end = str(sys.argv[2])
 buf = twi_graph.filter(lambda x: x[0] == start).map(lambda x: (x[1], start))
 buf = twi_graph.join(buf).map(lambda x: (x[1][0], (x[0], x[1][1])))
 k = 0
-while k <= 20:
+while k <= 30:
     buf = twi_graph.join(buf).map(lambda x: (x[1][0], (x[0], ) + x[1][1]))
     if buf.keys().filter(lambda x: x == end).count() > 0:
-        buf.filter(lambda x: x[0] == end).map(lambda x: ((x[0],) + x[1])[::-1])\
-                                         .map(lambda x: ','.join(x))
-        buf.saveAsTextFile(str(sys.argv[4]))
+        buf.filter(lambda x: x[0] == end).map(lambda x: ((x[0],) + x[1])[::-1]).map(lambda x: ','.join(x))
+        buf.saveAsTextFile(sys.argv[4])
         break
     k += 1
